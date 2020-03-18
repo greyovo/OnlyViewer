4b581357e8ae8692004d7dff24703a30aa2bb372
Kevin <we1017096141@qq.com>
2020-03-18 17:37:49 +0800

    修改ImageModel单元图片类

--- src/home/java/model/ImageModel.java
+++ src/home/java/model/ImageModel.java
@@ -2,14 +2,42 @@ package model;
 
 import lombok.Data;
 import org.junit.Test;
+import java.io.File;
+
+/**
+ * ProjName: OnlyViewer
+ * FileName: ImageModel
+ * Author: Kevin
+ * Describe: 单元图片类
+ */
 
 //lombok自动完成getter/setter/toString
 @Data
 public class ImageModel {
-    //单元测试
-    @Test
-    public void Test1(){
-        System.out.println("Test!");
+
+    private String imageFilePath; // *绝对路径
+    private File imageFile;
+    private String imageName;
+    private long imageLength;
+    private String imageType;
+
+    public ImageModel(String path){
+        this.imageFilePath = path; // 单个图片的绝对路径
+        this.imageFile = new File(path);
+        this.imageName = imageFile.getName();
+        this.imageLength = imageFile.length();
     }
 
+//    //单元测试
+//    @Test
+//    public void Test1(){
+//        // listFiles只会找该层级的文件/文件夹
+//        File file = new File("D:/Idea/Gitproj/OnlyViewer/src/home/java");
+//        File[] files = file.listFiles();
+//        assert files != null;
+//        for(File f: files){
+//            System.out.println(f.getName());
+//        }
+//        System.out.println("Test!");
+//    }
 }
