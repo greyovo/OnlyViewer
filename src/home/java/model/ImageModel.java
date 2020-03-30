484a2dd90aa6df262f6bcb479a46a88fdf30002f
Kevin <we1017096141@qq.com>
2020-03-30 22:07:37 +0800

    update imgSize与imgLastModified

--- src/home/java/model/ImageModel.java
+++ src/home/java/model/ImageModel.java
@@ -1,8 +1,10 @@
 package model;
 
 import lombok.Data;
-import org.junit.Test;
+
 import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * @ProjName: OnlyViewer
@@ -18,14 +20,42 @@ public class ImageModel {
     private String imageFilePath; // *绝对路径
     private File imageFile;
     private String imageName;
-    private long imageLength;
-    private String imageType;
+    private long fileLength;
+    private String imageSize;
+//    private String imageType; // 图片类型 依赖于ImageListModel
+    private String imageLastModified; // 图片修改时间
 
-    public ImageModel(String path){
-        this.imageFilePath = path; // 单个图片的绝对路径
-        this.imageFile = new File(path);
-        this.imageName = imageFile.getName();
-        this.imageLength = imageFile.length();
+    // 暂且先不考虑获取图片宽高，其耗时较多
+    private int imageWidth;
+    private int imageHeight;
+
+    public ImageModel(File file){
+        this.imageFile = file;
+        this.imageFilePath = file.getAbsolutePath();
+        this.imageName = file.getName();
+        this.fileLength = file.length();
+        this.imageSize = getStandardSize(fileLength);
+        this.imageLastModified = getStandardTime(imageFile.lastModified());
     }
 
+    public ImageModel(String path){
+        this.imageFilePath = path;
+        this.imageFile = new File(path);
+        this.imageName = imageFile.getName();
+        this.fileLength = imageFile.length(); // 返回的单位是byte
+        this.imageSize = getStandardSize(fileLength);
+        this.imageLastModified = getStandardTime(imageFile.lastModified());
+    }
+
+    public static String getStandardSize(long fileLength){
+        return GenUtilModel.getStandardSize(fileLength);
+    }
+
+    public static String getStandardTime(long time){
+        String imgStandardTime = null;
+        Date data = new Date(time);
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        imgStandardTime = sdf.format(data);
+        return imgStandardTime;
+    }
 }
