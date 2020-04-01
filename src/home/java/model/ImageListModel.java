81894f53c63f8dbf59b1a6e976c9605e7cf5251c
Kevin <we1017096141@qq.com>
2020-04-01 21:13:49 +0800

    update 优化获取列表性能

--- src/home/java/model/ImageListModel.java
+++ src/home/java/model/ImageListModel.java
@@ -3,105 +3,73 @@ package home.java.model;
 import lombok.Data;
 import org.junit.Test;
 
-import javax.imageio.ImageIO;
-import javax.imageio.ImageReader;
-import javax.imageio.stream.ImageInputStream;
-import java.io.File;
 import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
-import java.util.Iterator;
-import home.java.model.ImageModel;
 
 /**
  * @ProjName: OnlyViewer
  * @ClassName: ImageListModel
  * @Author: Kevin
  * @Time:2020/3/18 11:11
- * @Describe: 文件夹内的图片列表
- * 1.判断是否为图片 2.计算图片数 3.创建图片列表 4.计算文件夹所有图片的大小
+ * @Describe: 文件夹内的图片列表2.0
+ * 1.文件筛选 2.计算图片数 3.创建图片列表 4.计算文件夹所有图片的大小
  **/
 
 @Data
 public class ImageListModel {
 
-    // Image支持的五种格式
-    // 有多种方法检查 1.后缀 2. ImageIO 3.Iterator
-    // 1.简单地通过后缀判断不靠谱但是快
-    // 2.利用ImageIO检查图片格式，支持jpg/jpeg/png/gif/bmp
-    // 缺点：ImageIO在解码时涉及磁盘I/O，使得读取速度非常慢
-    // 3.利用ImageInputStream将图片变成输入流
-    // 利用ImageIO的getImageReaders返回一个迭代器
-    // 提高了程序效率
+//    private static String type;
+//
+//    private static int width;
+//    private static int height;
 
-    private static String type;
-
-    private static int width;
-    private static int height;
-
-    // 判断文件是否为图片 支持jpg/jepg/png/gif/bmp,暂不支持psd
-    private static boolean isSupportedImg(File file) throws IOException {
-        try{
-            ImageInputStream iis = ImageIO.createImageInputStream(file);
-            Iterator iterator = ImageIO.getImageReaders(iis);
-            if (!iterator.hasNext())
-                return false;
-            else{
-//                ImageReader ir = (ImageReader) iterator.next();
-//                ir.setInput(iis, true);
-//                type = ir.getFormatName();
-//                width = ir.getWidth(0);
-//                height = ir.getHeight(0);
-                iis.close();
-                return true;
-            }
-        }catch (IllegalArgumentException e){
-            // 修复Bug: 遇到系统文件应该进行异常捕获
-//            System.out.println("无效文件");
-            return false;
-        }
+    // 判断文件是否为图片 支持jpg/jpeg/png/gif/bmp,暂不支持psd
+    public static boolean isSupportedImg(String fileName){
+        return fileName.endsWith(".jpg") || fileName.endsWith(".jpeg") ||
+                fileName.endsWith(".png") || fileName.endsWith(".gif") ||
+                fileName.endsWith(".bmp");
     }
 
-    private static File[] files;
-    // 创建列表前需要判断当前文件夹里有没有照片,同时返回有多少张图片 必须要调用*
-    public static int calcImgNum(String path)  {
-        File file = new File(path);
-        files = file.listFiles(); // 主要是转成文件列表耗时
-        if (files == null){
-            return 0;
-        }
-        int num = 0;
-        for (File f : files){
-            try {
-                if (f.isFile() && isSupportedImg(f)){
-                    num++;
+    // 初始化图片列表
+    public static ArrayList<ImageModel> initImgList(String path) throws IOException{
+        ArrayList<ImageModel> imgList = new ArrayList<>(); // 默认根据name进行排序
+        Files.walkFileTree(Paths.get(path), new SimpleFileVisitor<Path>(){
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs){
+                String fileName = file.getFileName().toString().toLowerCase();
+                if (isSupportedImg(fileName)){
+                    imgList.add(new ImageModel(file.toString())); // 获取绝对路径
                 }
-            }catch (IOException e){
-                System.out.println("ImageIO Input Error!");
+                return FileVisitResult.CONTINUE;
             }
-        }
-        return num;
+
+            // 只访问当前文件夹 不进行递归访问
+            @Override
+            public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs){
+                    if (dir.toString().equals(path)){
+                        return FileVisitResult.CONTINUE;
+                    }else
+                        return FileVisitResult.SKIP_SUBTREE;
+            }
+
+            // 处理访问系统文件的异常
+            @Override
+            public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException{
+                System.out.println("不访问系统文件夹");
+                return FileVisitResult.SKIP_SUBTREE;
+            }
+        });
+        return imgList;
     }
 
-
-    // init前提:文件夹里有image
-    public static ArrayList<ImageModel> initImgList(int num){
-        ArrayList<ImageModel> imageList = new ArrayList<>(); // 初始化
-        for (int i=0; i<num; i++){
-            try {
-                if (files[i].isFile() && isSupportedImg(files[i])){
-                    ImageModel img = new ImageModel(files[i].getAbsolutePath());
-//                    img.setImageType(type);
-                    imageList.add(img);
-                }
-            }catch (IOException e){
-                System.out.println("ImageIO Input Error!");
-            }
-        }
-        System.out.println("init ImageList successfully!");
-        return imageList;
+    // 返回文件夹内图片张数
+    public static int getListImgNum(ArrayList<ImageModel> im){
+        return im.size();
     }
 
-    // 获取这个文件夹里的 *图片*大小 不包括其他文件夹的大小
+    // 返回文件夹内的 图片 大小
     public static String getListImgSize(ArrayList<ImageModel> im){
         long totalSize = 0;
         for (ImageModel i:im){
@@ -110,31 +78,43 @@ public class ImageListModel {
         return GenUtilModel.getFormatSize(totalSize);
     }
 
-    // TODO 刷新文件夹功能
-
+    // 刷新文件夹 返回新列表
+    public static ArrayList<ImageModel> refreshList(String path){
+        ArrayList<ImageModel> list = new ArrayList<>();
+        try{
+            list = initImgList(path);
+        }catch (IOException e){
+            System.out.println("IOException");
+        }
+        return list;
+    }
 
     @Test
-    public void Test1(){
-        // [830MB, 350张] - 3s800ms
-        // [153MB, 140张] - 1s300ms
-        // ... 不涉及图片的read操作，时间复杂度与图片数量呈正相关
-
-        String filePath = "F:\\Ding\\Ding_photo\\Phone Photo\\test";
-
-        int imageNum = calcImgNum(filePath);
-        if (imageNum==0){
+    public void Test1() throws IOException {
+        String filePath = "F:\\Ding\\LIFE\\phone";
+        long timef;
+        long timel;
+        timef = System.currentTimeMillis();
+        ArrayList<ImageModel> list = initImgList(filePath);
+        timel = System.currentTimeMillis();
+        System.out.printf("初始化耗时：%d ms\n", (timel-timef));
+        if (list.size()==0){
             System.out.println("There is no image!");
-        }
-        else if (imageNum>0){
-            ArrayList<ImageModel> list = initImgList(imageNum);
-            for (ImageModel i : list){
-                System.out.println("imgName:"+i.getImageName()+
-                        "\t\timgLastModified:"+i.getFormatTime()+
-                        "\t\timgSize:"+i.getFormatSize());
-            }
-            System.out.println("totalImgNum:" + imageNum);
+        }else {
+//            for (ImageModel i : list){
+//                System.out.println("imgName:"+i.getImageName()+
+//                        "\t\timgLastModified:"+i.getFormatTime()+
+//                        "\t\timgSize:"+i.getFormatSize());
+//            }
+            System.out.println("totalImgNum:" + getListImgNum(list));
             System.out.println("totalImgSize:" + getListImgSize(list));
+//            timef = System.currentTimeMillis();
+//            refreshList(filePath);
+//            System.out.println("刷新成功！");
+//            timel = System.currentTimeMillis();
+//            System.out.printf("耗时：%d ms\n", (timel-timef));
         }
+
         System.out.println("测试成功!");
     }
 }
