98635954a49bfad24f9df5a8b8179ecce1236e34
Kevin <we1017096141@qq.com>
2020-04-13 20:35:36 +0800

    修改了鼠标的点击响应事件

--- src/home/java/components/ImageBox.java
+++ src/home/java/components/ImageBox.java
@@ -3,15 +3,18 @@ package home.java.components;
 import com.jfoenix.controls.JFXRippler;
 import display.DisplayWindow;
 import home.java.model.ImageModel;
+import home.java.model.SelectedModel;
 import javafx.geometry.Pos;
-import javafx.scene.control.Label;
-import javafx.scene.control.Tooltip;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseButton;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import javax.tools.Tool;
+
 
 /**
  * 用来存放图片和图片文件名的盒子。
@@ -22,14 +25,12 @@ import javax.tools.Tool;
 public class ImageBox extends VBox {
 
     {
-        setMaxSize(170,170);
+        setMaxSize(170, 170);
         setAlignment(Pos.BOTTOM_CENTER);
     }
 
 
-
-
-    public ImageBox(ImageModel im){
+    public ImageBox(ImageModel im) {
         ImageView2 imageView = new ImageView2(new Image(im.getImageFile().toURI().toString(),
                 120,
                 120,
@@ -38,20 +39,30 @@ public class ImageBox extends VBox {
                 true));
         RipplerImageView riv = new RipplerImageView(imageView); //一个水波纹点击效果的包装
         ImageLabel imageLabel = new ImageLabel(im.getImageName()); //标签 - 文件名
-        this.getChildren().addAll(riv,imageLabel);
+        this.getChildren().addAll(riv, imageLabel);
 
         //设置文件信息tips
-        String tooltip = String.format("名称: %s\n大小: %s",im.getImageName(),im.getFormatSize());
+        String tooltip = String.format("名称: %s\n大小: %s", im.getImageName(), im.getFormatSize());
         Tooltip.install(this, new Tooltip(tooltip));
 
         setOnMouseClicked(event -> {
-            DisplayWindow dw = new DisplayWindow();
-            try {
-                dw.setImage(new Image(im.getImageFile().toURI().toString()));
-                dw.start(new Stage());
-            } catch (Exception e) {
-                e.printStackTrace();
+            if (event.getButton() == MouseButton.PRIMARY && event.getClickCount() == 1) {
+                // TODO 鼠标左键单击图片显示选中框
+
+            } else if (event.getButton() == MouseButton.PRIMARY && event.getClickCount() == 2) {
+                // 鼠标左键双击看大图
+                DisplayWindow dw = new DisplayWindow();
+                try {
+                    dw.setImage(new Image(im.getImageFile().toURI().toString()));
+                    dw.start(new Stage());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            } else if (event.getButton() == MouseButton.SECONDARY) {
+                // TODO 鼠标右键菜单
+
             }
+
         });
 
     }
