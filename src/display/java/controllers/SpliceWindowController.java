05f4250ad2beefc26a5fc06c8eedfe0ee63cce1f
tudoudaren233 <522363019@qq.com>
2020-05-11 21:44:55 +0800

    截图方法待实装

--- src/display/java/controllers/SpliceWindowController.java
+++ src/display/java/controllers/SpliceWindowController.java
@@ -11,13 +11,13 @@ import java.io.IOException;
 
 public class SpliceWindowController {
     private void snap(ImageView imageView){
-        //此处当把imageview放到scene中则不会报错
-        WritableImage wa = imageView.getParent().snapshot(null,null);
-        try {
-            BufferedImage buff = SwingFXUtils.fromFXImage(wa,null);
-            ImageIO.write(buff,"png",new File("test"  + ".png"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        //将下面解除注释！！！当把imageview放到scene中则不会报错
+        //WritableImage wa = imageView.getParent().snapshot(null,null);
+//        try {
+//            BufferedImage buff = SwingFXUtils.fromFXImage(wa,null);
+//            ImageIO.write(buff,"png",new File("test"  + ".png"));
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
     }
 }
