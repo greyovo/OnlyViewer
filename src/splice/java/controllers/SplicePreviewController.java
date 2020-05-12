cf83aa6d9d38f2a00afec549ea55dc56de98e714
tudoudaren233 <522363019@qq.com>
2020-05-12 16:48:14 +0800

    关闭窗口避开卡顿，拼接图像存到当前目录

--- src/splice/java/controllers/SplicePreviewController.java
+++ src/splice/java/controllers/SplicePreviewController.java
@@ -3,6 +3,8 @@ package splice.java.controllers;
 import com.jfoenix.controls.JFXButton;
 import home.java.controllers.ControllerUtil;
 import home.java.model.ImageModel;
+import home.java.model.SelectedModel;
+import javafx.application.Platform;
 import javafx.embed.swing.SwingFXUtils;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -13,13 +15,18 @@ import javafx.scene.image.ImageView;
 import javafx.scene.image.WritableImage;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.ResourceBundle;
 import java.util.Set;
 
@@ -35,6 +42,8 @@ public class SplicePreviewController implements Initializable {
     private ScrollPane scrollPane;
     @FXML
     private VBox vBox;
+
+    private ImageModel imageModel;
 
 //    @FXML
 //    private Image image1;
@@ -65,17 +74,24 @@ public class SplicePreviewController implements Initializable {
     public void setImageModelSet(Set<ImageModel> set) {
         this.imageModelSet = set;
         scrollPane.setContent(vBox);
-
+        int number = 0;
         //将图片加入到垂直盒子中
         for (ImageModel im : imageModelSet) {
             Image image = new Image(im.getImageFile().toURI().toString());
             ImageView imageView = new ImageView(image);
+            if (number==0){
+                this.imageModel = im;
+                this.imageView = imageView;
+                number++;
+            }
             imageView.setSmooth(true);
             imageView.setFitWidth(800);     //此处指定了拼接出的图片的宽度，注释掉此句则保持原图尺寸
             imageView.setPreserveRatio(true);
             imageView.setStyle("-fx-margin:0;-fx-padding:0;");
             vBox.getChildren().add(imageView);
         }
+
+
     }
 
 
@@ -85,11 +101,24 @@ public class SplicePreviewController implements Initializable {
     // FIXME: 2020/5/12 应当保存到图片所在的目录
     private void snap() {
         WritableImage wa = imageView.getParent().snapshot(null, null);
-        try {
-            BufferedImage buff = SwingFXUtils.fromFXImage(wa, null);
-            ImageIO.write(buff, "png", new File("test" + ".png"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        //设置图片名字为当前系统时间
+        Date date = new Date();
+        File tf;
+        SimpleDateFormat dateFormat= new SimpleDateFormat("yyyy_MM_dd_HH_mm");
+            try {
+                BufferedImage buff = SwingFXUtils.fromFXImage(wa, null);
+                ImageIO.write(buff, "png",
+                        //tf = new File(dateFormat.format(date) + "拼接"+".png"));
+                        //保存到当前文件夹
+                       new File(imageModel.getImageParentPath()+"\\" +"拼接"+ dateFormat.format(date) + ".png"));
+                //为了处理卡顿关闭该窗口
+                Stage stage =(Stage) imageView.getScene().getWindow();
+                stage.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
     }
+
 }
